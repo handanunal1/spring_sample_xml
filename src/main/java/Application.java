9a74288add380db2e02ee900b447e1f55c49243e
handanunal <handan.unal@yahoo.com>
2019-10-15 20:29:56 +0300

    singleton

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+//CustomerService service = (CustomerService) new CustomerServiceImpl();
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service);
+		System.out.println(service.findAll().get(0).getFirstName());
+		CustomerService service2 = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service2);
+
+	}
+
+}
